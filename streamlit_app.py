3c4f1cd6d3fc0968647777a4abb2588134bc6761
Paras140790 <139624639+Paras140790@users.noreply.github.com>
2023-07-15 16:41:37 +0530

    Update streamlit_app.py
    Added three dishes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
+streamlit.text('Palak Paneer')
+streamlit.text('Matar Paneer')
+streamlit.text('Butter Paneere')
